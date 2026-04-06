88abcc4571c35b9caae87dc739c19683db37a7df
Sarthakpatil23 <sarthakpatil2194@gmail.com>
2026-04-06 20:12:35 +0530

    Add favicon SVG for the application

--- talvo/urls.py
+++ talvo/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.templatetags.static import static as static_url
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=static_url('talvo1/images/favicon.svg'), permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('talvo1.urls')),
